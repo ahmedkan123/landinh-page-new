efc628bd3ffccc54b82b4f0c47c8d40bbbda4911
ahmed <ahmedabdelgawad951@gmail.com>
2021-10-01 16:06:10 +0200

    last update

--- js/app.js
+++ js/app.js
@@ -18,7 +18,7 @@
  *
  */
 var sections = document.querySelectorAll("section");
-var lengthSections = sections.length;
+const lengthSections = sections.length;
 //  console.log(lengthSections);
 /**
  * End Global Variables
@@ -33,7 +33,24 @@ var lengthSections = sections.length;
  */
 
 // build the nav
+// const hamburger = document.querySelector(".hamburger");
+// const navMenu = document.querySelector(".navbar__list");
 
+// hamburger.addEventListener("click", mobileMenu);
+
+// function mobileMenu() {
+//     hamburger.classList.toggle("active");
+//     navMenu.classList.toggle("active");
+// }
+
+// const navLink = document.querySelectorAll("a");
+
+// navLink.forEach(n => n.addEventListener("click", closeMenu));
+
+// function closeMenu() {
+//     hamburger.classList.remove("active");
+//     navMenu.classList.remove("active");
+// }
 // Add class 'active' to section when near top of viewport
 
 // Scroll to anchor ID using scrollTO event
@@ -50,34 +67,16 @@ var navbar = document.querySelector("nav #navbar__list");
 
 for (let i = 1; i <= lengthSections; i++) {
   var liElement = document.createElement("li");
-  //   var sectionId = sections[i].getAttribute("id");
   navbar.appendChild(liElement);
   liElement.innerHTML = `<a href="#section${i}" class="menu__link">Section ${i}</a>`;
 }
 
-// Scroll to section on link click
-// function scrollToSection() {
-//   var linkClick = document.querySelector("nav a");
-//   linkClick.forEach(a => {
-//     a.addEventListener("click", function(e) {
-//       e.preventDefault();
-//     //   var hrefElement = a.getAttribute("href");
-//       var sectionId = document.querySelector(a.getAttribute("href"));
-//       sectionId.scrollIntoView({
-//         behavior: "smooth",
-//         block: "start",
-//         inline: "nearest",
-//       });
-//     });
-//   });
-// }
-// scrollToSection();
+// Scroll to anchor ID when click link using scrollIntoView
+
 let linkClick = document.querySelectorAll("nav a");
-// console.log(linkClick);
 linkClick.forEach((a) => {
   a.addEventListener("click", function (e) {
     e.preventDefault();
-    //   var hrefElement = a.getAttribute("href");
     var sectionId = document.querySelector(a.getAttribute("href"));
     sectionId.scrollIntoView({
       behavior: "smooth",
@@ -87,46 +86,35 @@ linkClick.forEach((a) => {
   });
 });
 
-// linkClick.
+// when click on link is active
+var ul = document.getElementById("navbar__list");
+var liLinks = ul.querySelectorAll("li");
+liLinks.forEach((li) => {
+  li.addEventListener("click", function () {
+    liLinks.forEach((liElem) => {
+      liElem.classList.remove("active");
+    });
+    this.classList.add("active");
+  });
+});
 
 // Set sections as active
-
-// var sections = document.querySelectorAll("section");
+// when scroll add active class for section and link
 window.onscroll = function () {
   sections.forEach((section) => {
-    // var current = "";
-
     if (
       section.getBoundingClientRect().top >= -400 &&
       section.getBoundingClientRect().top <= 150
     ) {
-      // current = section.getAttribute("id");
-      // console.log(current);
-      section.classList.add("active");
+      section.classList.add("your-active-class");
+      var currentId = section.getAttribute("id");
+      document.querySelectorAll("nav a").forEach((el) => {
+        el.classList.remove("active");
+      });
+      var selector = `nav a[href="#${currentId}"]`;
+      document.querySelector(selector).classList.add("active");
     } else {
-      section.classList.remove("active");
+      section.classList.remove("your-active-class");
     }
   });
-  // linkClick.forEach((link) => {
-  //   link.classList.remove("active1");
-  //   if (link.classList.contains(current)) {
-  //     link.classList.add("active1");
-  //   }
-  // });
 };
-// window.onscroll = () => {
-//   var current = "";
-
-//   sections.forEach((section) => {
-//     const sectionTop = section.offsetTop;
-//     if (scrollY >= sectionTop - 60) {
-//       current = section.getAttribute("id"); }
-//   });
-
-//   linkClick.forEach((a) => {
-//     a.classList.remove("active1");
-//     if (a.classList.contains(current)) {
-//       a.classList.add("active1");
-//     }
-//   });
-// };
